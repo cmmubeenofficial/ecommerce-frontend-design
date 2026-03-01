4bf1041778765afdf199758577e1c045fb663907
CM Mubeen <cmmubeen@gmail.com>
2026-03-01 21:46:33 +0500

    bellow pagination UI completed and product listing also completed with this

--- src/components/ProductListingPage/ProductListing/ProductListing.jsx
+++ src/components/ProductListingPage/ProductListing/ProductListing.jsx
@@ -1,4 +1,4 @@
-import { faAngleDown } from "@fortawesome/free-solid-svg-icons"
+import { faAngleDown, faAngleLeft, faAngleRight } from "@fortawesome/free-solid-svg-icons"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import SelectedFilters from "./SelectedFilters"
 import { selectedFiltersData } from "./selectedFiltersData"
@@ -59,8 +59,51 @@ function ProductListing() {
       {/* product cards */}
       <div className="product-cards-container flex flex-wrap gap-4.5">
         {productCardData.map(productDetail => (
-        <ProductCard key={productDetail.id} productDetails={productDetail} />
-      ))}
+          <ProductCard key={productDetail.id} productDetails={productDetail} />
+        ))}
+      </div>
+
+      {/* bottom pagination */}
+      <div className="pagination-container flex justify-end items-center gap-2 mt-4 w-230.25">
+
+        {/* Show count dropdown */}
+        <div className="relative">
+          <select className="appearance-none h-10 w-32 border border-[#DEE2E7] rounded-md px-3 bg-white cursor-pointer pr-8">
+            <option value="10">Show 10</option>
+            <option value="20">Show 20</option>
+          </select>
+          <FontAwesomeIcon
+            icon={faAngleDown}
+            className="text-[#8B96A5] absolute right-3 top-1/2 -translate-y-1/2 pointer-events-none"
+          />
+        </div>
+
+        {/* Page numbers group */}
+        <div className="flex border border-[#DEE2E7] rounded-md overflow-hidden bg-white">
+
+          {/* Left Arrow */}
+          <button className="h-10 w-11 flex items-center justify-center border-r border-[#DEE2E7] hover:bg-gray-50 cursor-pointer">
+            <FontAwesomeIcon icon={faAngleLeft} className="text-[#1C1C1C]" />
+          </button>
+
+          {/* Pages */}
+          <ul className="flex items-center">
+            <li className="h-10 w-11 flex items-center justify-center border-r border-[#DEE2E7] bg-[#EFF2F4] font-semibold text-[#1C1C1C]">
+              1
+            </li>
+            <li className="h-10 w-11 flex items-center justify-center border-r border-[#DEE2E7] hover:bg-gray-50 cursor-pointer">
+              2
+            </li>
+            <li className="h-10 w-11 flex items-center justify-center border-r border-[#DEE2E7] hover:bg-gray-50 cursor-pointer">
+              3
+            </li>
+          </ul>
+
+          {/* Right Arrow */}
+          <button className="h-10 w-11 flex items-center justify-center hover:bg-gray-50 cursor-pointer">
+            <FontAwesomeIcon icon={faAngleRight} className="text-[#1C1C1C]" />
+          </button>
+        </div>
       </div>
     </div>
   )
